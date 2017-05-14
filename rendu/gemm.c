93c0a762933faeb775cc3804295cc31f468b37cd
ASGdev <aurelien.surier@gmail.com>
2017-05-14 10:38:37 +0200

    Ajout dernière version de gemm

--- rendu/gemm.c
+++ rendu/gemm.c
@@ -1,8 +1,57 @@
+#include <stdlib.h>
+
 #include "mnblas.h"
-#include <x86intrin.h>
+
+#include <nmmintrin.h>
+#include <stdio.h>
+
+#define VEC_SIZE 4
 
 typedef float *floatM;
 typedef double *doubleM;
+
+typedef float float4 [4]  __attribute__ ((aligned (16))) ;
+typedef double double2 [2] __attribute__ ((aligned (16))) ;
+
+typedef struct {
+  float REEL;
+  float IMAG;
+}vcomplexe;
+
+typedef vcomplexe VCOMP [VEC_SIZE] ;
+
+typedef struct {
+  double REEL;
+  double IMAG;
+}dcomplexe;
+
+typedef dcomplexe DCOMP [VEC_SIZE] ;
+
+
+// typedef float matrix [4][4] ;
+// typedef double matrix [4][4] ;
+typedef vcomplexe matrix[2][2];
+
+void print_matrix (matrix M, int N)
+{
+  register unsigned int i, j ;
+
+  for (i = 0 ; i < N; i++)
+    {
+      for (j = 0 ; j < N; j++)
+	{
+	  printf (" %3.2f ", M[i][j]) ;
+	}
+      printf ("\n") ;
+    }
+  printf ("\n") ;
+  return ;
+}
+
+void print_(double *m){
+	for(int i = 0; i< 16; i++)
+		printf("%f ", *(m+i));
+}
 
 void mncblas_sgemm (
               MNCBLAS_LAYOUT layout, MNCBLAS_TRANSPOSE TransA,
@@ -602,7 +651,7 @@ void mncblas_cgemm (
   return ;
 }
 
-void mncblas_cgemm_vec(MNCBLAS_LAYOUT layout, MNCBLAS_TRANSPOSE TransA,
+void mncblas_cgemm_vec (MNCBLAS_LAYOUT layout, MNCBLAS_TRANSPOSE TransA,
        MNCBLAS_TRANSPOSE TransB, const int M, const int N,
        const int K, const void *alpha, const void *A,
        const int lda, const void *B, const int ldb,
@@ -613,93 +662,107 @@ void mncblas_cgemm_vec(MNCBLAS_LAYOUT layout, MNCBLAS_TRANSPOSE TransA,
     vectorized implementation
   */
   
- //  register unsigned int i ;
- //  register unsigned int j ;
- //  register unsigned int k ;
- //  register unsigned int l ;
+  register unsigned int i ;
+  register unsigned int j ;
+  register unsigned int k ;
+  register unsigned int l ;
 
- //  register unsigned int indice_ligne ;
+  register unsigned int indice_ligne ;
   
- //  register float r ;
- //  floatM   Bcol ;
- //  float4   R4 ;
- //  int      err ;
- //  float4 tres;
+  register float r ;
+  floatM   Bcol ;
+  float4   R4 ;
+  int      err ;
+  float4 tres;
 
- //  float *AP = (float *)A;
- //  float *BP = (float *)B;
- //  float *CP = (float *)C;
- //  float *bv = (float *)beta;
- //  float *av = (float *)alpha;
+  float *AP = (float *)A;
+  float *BP = (float *)B;
+  float *CP = (float *)C;
+  float *bv = (float *)beta;
+  float *av = (float *)alpha;
+
+  for(int u = 0 ; u < M*M*2; u++){
+    printf("%f ", *(BP + u));
+  }
+  printf("\n");
   
- //  __m128 av4 ;
- //  __m128 bv4 ;
- //  __m128 dot ;
- //  __m128 alphav;
- //  __m128 betav;
- //  __m128 rv;
+  __m128 av4 ;
+  __m128 bv4 ;
+  __m128 dot ;
+  __m128 alphav;
+  __m128 betav;
+  __m128 rv;
 
- //  // load alpha & beta
- // alphav = _mm_set_ps(*(av), *(av), *(av+1), *(av+1));
- // betav = _mm_set_ps(*(bv), *(bv), *(bv+1), *(bv+1));
-
-
- //  /* NoTrans only */
-
- //  Bcol = aligned_alloc (16, M * sizeof (double)) ;
- //  //printf("M = %d\n", M);
- //  for (i = 0 ; i < M; i = i + 1) {
-
- //    for (j = 0 ; j < M; j ++) {
- //          /*
- //      load a B column (j)
- //          */
- //        for (l = 0 ; l < M ; l = l + 4)
- //        {
- //          // complexe 1
- //          Bcol [l]     = BP [l        * M + j ] ;
- //          Bcol [l + 1] = BP [(l + 1)  * M + j ] ;
- //          // complexe 2
- //          Bcol [l + 2] = BP [(l + 2)  * M + j ] ;
- //          Bcol [l + 3] = BP [(l + 3)  * M + j ] ;
- //        }
- //        for(int e = 0; e<M; e++){
- //          //printf("%f ", Bcol[i]);
- //        }
-
- //        r = 0.0 ;
- //        indice_ligne = i * M ;
-
- //        // for (k = 0; k < M; k = k + 2)
- //        // {
-
- //        //   av4 = _mm_load_ps ((AP+indice_ligne + k));
- //        //   bv4 = _mm_load_ps (Bcol+ k) ;
-
- //        //   dot = _mm_dp_ps (av4, bv4, 0xFF) ;
-
- //        //   _mm_store_ps (R4, dot) ;
-
- //        //   r = r + R4 [0] ;
- //        // }
-
- //        // alpha * r
- //        __m128 ar = _mm_mul_ps(alphav, rv);
- //        ar = _mm_addsub_ps(ar, _mm_shuffle_ps(ar, ar, _MM_SHUFFLE(0, 0, 3, 2)));
-
- //        // beta * C
- //        float4 cl;
- //        cl[0] = cl[1] = CP[indice_ligne + j];
- //        cl[2] = cl[3] = CP[indice_ligne + j + 1];
- //        __m128 cv = _mm_load_ps(cl);
- //        __m128 bc = _mm_mul_ps(betav, cv);
- //        bc = _mm_addsub_ps(bc, _mm_shuffle_ps(bc, bc, _MM_SHUFFLE(0, 0, 3, 2)));
+  // load alpha & beta
+ alphav = _mm_set_ps(*(av+1), *(av+1), *(av), *(av));
+ betav = _mm_set_ps(*(bv+1), *(bv+1), *(bv), *(bv));
 
 
- //        CP [indice_ligne + j] = ar[0] + bc[0];
- //        CP [indice_ligne + j + 1] = ar[1] + bc[1];
- //      }
- //  }
+  /* NoTrans only */
+
+  Bcol = aligned_alloc (16, M * sizeof (double)) ;
+  printf("M = %d\n", M);
+  for (i = 0 ; i < M*2; i = i + 1) {
+
+    for (j = 0 ; j < M*2; j += 2) {
+
+          /*
+      load a B column (j)
+          */
+        for (l = 0 ; l < M ; l = l + 4)
+        {
+          Bcol [l]     = BP [l        * M + j ] ;
+          Bcol [l + 1] = BP [(l  * M + j) + 1] ;
+          Bcol [l + 2] = BP [(l + 2)  * M + j ] ;
+          Bcol [l + 3] = BP [((l + 2) * M + j) + 1 ] ;
+
+        }
+        for(int e = 0; e<M*2; e++){
+          printf("%f ", Bcol[e]);
+        }
+        printf("-\n");
+
+        r = 0.0 ;
+        indice_ligne = i * M ;
+
+        for (k = 0; k < M*2; k = k + 4)
+        {
+
+          float4 t;
+          t[0] = AP[indice_ligne + k + 0];
+          t[1] = AP[indice_ligne + k + 1];
+          t[2] = AP[indice_ligne + k + 2];
+          t[3] = AP[indice_ligne + k + 3];
+          av4 = _mm_load_ps (t);
+          bv4 = _mm_load_ps (Bcol+ k) ;
+
+          dot = _mm_dp_ps (av4, bv4, 0xFF) ;
+
+          _mm_store_ps (R4, dot) ;
+
+          r = r + R4 [0] ;
+        }
+
+        rv = _mm_set_ps(r, r, r, r);
+        // alpha * r
+        __m128 ar = _mm_mul_ps(alphav, rv);
+        ar = _mm_addsub_ps(ar, _mm_shuffle_ps(ar, ar, _MM_SHUFFLE(0, 0, 3, 2)));
+
+        // beta * C
+        float4 cl;
+        cl[0] = cl[1] = CP[indice_ligne + j];
+        cl[2] = cl[3] = CP[indice_ligne + j + 1];
+        __m128 cv = _mm_load_ps(cl);
+        __m128 bc = _mm_mul_ps(betav, cv);
+        bc = _mm_addsub_ps(bc, _mm_shuffle_ps(bc, bc, _MM_SHUFFLE(0, 0, 3, 2)));
+
+        
+        printf("indice_ligne = %d , j = %d\n", indice_ligne, j);
+
+        CP [indice_ligne + j] = ar[0] + bc[0];
+        CP [indice_ligne + j + 1] = ar[1] + bc[1];
+      }
+  }
   return ;
 }
 
@@ -727,3 +790,134 @@ void mncblas_zgemm_vec (
   return ;
 }
 
+void mncblas_cgemm_vec_new (
+		    MNCBLAS_LAYOUT layout, MNCBLAS_TRANSPOSE TransA,
+       MNCBLAS_TRANSPOSE TransB, const int M, const int N,
+       const int K, const void *alpha, const void *A,
+       const int lda, const void *B, const int ldb,
+       const void *beta, void *C, const int ldc
+		   )
+{
+  /*
+    vectorized implementation
+  */
+  
+  register unsigned int i ;
+  register unsigned int j ;
+  register unsigned int k ;
+  register unsigned int l ;
+
+  register unsigned int indice_ligne ;
+  
+  register float r ;
+  floatM   Bcol ;
+  float4   R4 ;
+  int      err ;
+
+  __m128 av4 ;
+  __m128 bv4 ;
+  __m128 dot ;
+  __m128 rv;
+
+  float *AP = (float *)A;
+  float *BP = (float *)B;
+  float *CP = (float *)C;
+  float *bv = (float *)beta;
+  float *av = (float *)alpha;
+
+  bv4 = _mm_set_ps(*(bv+1), *(bv+1), *(bv), *(bv));
+  av4 = _mm_set_ps(*(av+1), *(av+1), *(av), *(av));
+
+  Bcol = aligned_alloc (16, M * sizeof (float)) ;
+  
+  for (i = 0 ; i < M*M; i = i + 1)
+	{
+	  
+	  for (j = 0 ; j < M; j ++)
+	    {
+
+	      /*
+		load a B column (j)
+	      */
+
+	      for (l = 0 ; l < M ; l = l + 4)
+        {
+          Bcol [l]     = BP [l        * M + j ] ;
+          Bcol [l + 1] = BP [(l  * M + j) + 1] ;
+          Bcol [l + 2] = BP [(l + 2)  * M + j ] ;
+          Bcol [l + 3] = BP [((l + 2) * M + j) + 1 ] ;     
+        }
+
+	      r = 0.0 ;	  
+	      indice_ligne = i * M ;
+	  
+	      for (k = 0; k < M*M; k = k + 2)
+        {
+          float4 temp;
+          temp[0] = temp[2] = AP[indice_ligne + k];
+          temp[1] = temp[3] = AP[indice_ligne + k + 1];
+          
+          av4 = _mm_load_ps (temp);
+
+          float4 temp2;
+          temp2[0] = temp2[2] = Bcol[0];
+          temp2[1] = temp2[3] = Bcol[1];
+          bv4 = _mm_load_ps (temp2) ;
+          
+          dot = _mm_dp_ps (av4, bv4, 0xFF) ;
+            
+          _mm_store_ps (R4, dot) ;
+
+          r = r + R4 [0] ;
+        }
+        //printf("-> %d\n", indice_ligne+j);
+	      //CP [indice_ligne + j] = (alpha * r) + (beta * C [indice_ligne + j]) ;
+
+        // alpha * r
+        rv = _mm_set_ps(r, r, r, r);
+        __m128 ar = _mm_mul_ps(av4, rv);
+        ar = _mm_addsub_ps(ar, _mm_shuffle_ps(ar, ar, _MM_SHUFFLE(0, 0, 3, 2)));
+        float4 arf;
+        _mm_store_ps(arf, ar);
+
+        // beta * C
+        float4 cl;
+        cl[0] = cl[1] = CP[indice_ligne + j];
+        cl[2] = cl[3] = CP[indice_ligne + j + 1];
+        __m128 cv = _mm_load_ps(cl);
+        __m128 bc = _mm_mul_ps(bv4, cv);
+        bc = _mm_addsub_ps(bc, _mm_shuffle_ps(bc, bc, _MM_SHUFFLE(0, 0, 3, 2)));
+        float4 bcf;
+        _mm_store_ps(bcf, bc);
+
+        CP [indice_ligne + j] = arf[0] + bcf[0] + CP [indice_ligne + j];
+        CP [indice_ligne + j + 1] = arf[1] + bcf[1] + CP [indice_ligne + j + 1];
+	    }
+	}
+   
+  return ;
+}
+
+int main(){
+
+
+  matrix A = {
+    {{1.0, 2.0}, {1.0,2.0}},
+    {{1.0,2.0}, {1.0,2.0}}
+  };
+  matrix B = {
+    {{1.0,2.0}, {1.0,2.0}},
+    {{1.0,2.0}, {1.0,2.0}}
+  };
+  matrix C1 = {
+    {{1.0,2.0}, {1.0,2.0}},
+    {{1.0,2.0}, {1.0,2.0}}
+  };
+
+  vcomplexe alpha = {1.0,2.0};
+  vcomplexe beta = {1.0,2.0};
+
+  mncblas_cgemm_vec (101, 111, 111, 2, 2, 2, &alpha, *A, 1, *B, 1, &beta, *C1, 1);
+
+	return 0;
+}
